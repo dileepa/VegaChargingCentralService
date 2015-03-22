0da2ce8769d83552488a052a3cca0693ec9e32f2
dileepa <dileepasisila@gmail.com>
2015-03-22 10:07:39 +0530

    State machine  Implementation

--- modules/service/manager/src/main/java/lk/vega/charger/centralservice/service/transaction/statecontroller/TransactionStartedState.java
+++ modules/service/manager/src/main/java/lk/vega/charger/centralservice/service/transaction/statecontroller/TransactionStartedState.java
@@ -4,6 +4,9 @@ import lk.vega.charger.centralservice.service.StartTransactionRequest;
 import lk.vega.charger.centralservice.service.transaction.TransactionController;
 import lk.vega.charger.core.ChargeTransaction;
 import lk.vega.charger.util.ChgResponse;
+import lk.vega.charger.util.CoreController;
+
+import java.sql.SQLException;
 
 /**
  * Created by Dileepa on 3/21/15.
@@ -15,10 +18,25 @@ public class TransactionStartedState implements TransactionState
     @Override
     public ChgResponse proceedState(TransactionContext transactionContext)
     {
+        ChgResponse chgResponse = null;
         StartTransactionRequest startTransactionRequest = transactionContext.getStartTransactionRequest();
         ChargeTransaction chargeTransaction = TransactionController.generateTransaction( startTransactionRequest );
         chargeTransaction.setTransactionStatus(TransactionController.TRS_STARTED );
+        try
+        {
+            chgResponse = CoreController.save(chargeTransaction);
+        }
+        catch (SQLException sq)
+        {
+            sq.printStackTrace();
+            chgResponse = new ChgResponse(ChgResponse.ERROR,sq.getMessage());
+        }
+        catch (Exception e)
+        {
+           e.printStackTrace();
+            chgResponse = new ChgResponse(ChgResponse.ERROR,e.getMessage())
+        }
         //TODO ChargeTransaction Save Call Should be here..
-        return  null;
+        return  chgResponse;
     }
 }
