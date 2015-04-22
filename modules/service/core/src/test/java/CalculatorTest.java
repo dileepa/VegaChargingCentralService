d350fe4456ec5fa5022c8508991c454abc234179
dileepa <dileepasisila@gmail.com>
2015-04-22 11:47:58 +0530

    Added Stub Generation Project for Mobitel M Cash Implementation

--- modules/service/core/src/test/java/CalculatorTest.java
+++ modules/service/core/src/test/java/CalculatorTest.java
@@ -1,114 +1,114 @@
-/**
-* Created by dileepa on 4/7/15.
-*/
-import lk.vega.charger.util.calculation.PriceCalculator;
-import org.drools.RuleBase;
-import org.drools.RuleBaseFactory;
-import org.drools.WorkingMemory;
-import org.drools.compiler.DroolsError;
-import org.drools.compiler.DroolsParserException;
-import org.drools.compiler.PackageBuilder;
-import org.drools.compiler.PackageBuilderErrors;
-import org.drools.rule.Package;
-import org.junit.Test;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-
-public class CalculatorTest
-{
-
-    @Test
-    public void shouldFireHelloWorld() throws IOException, DroolsParserException {
-        RuleBase ruleBase = initialiseDrools();
-        WorkingMemory workingMemory = initializeMessageObjects(ruleBase);
-        int expectedNumberOfRulesFired = 1;
-
-        int actualNumberOfRulesFired = workingMemory.fireAllRules();
-
-        assertThat(actualNumberOfRulesFired, is(expectedNumberOfRulesFired));
-    }
-
-    private RuleBase initialiseDrools() throws IOException, DroolsParserException {
-        PackageBuilder packageBuilder = readRuleFiles();
-        return addRulesToWorkingMemory(packageBuilder);
-    }
-
-    private PackageBuilder readRuleFiles() throws DroolsParserException, IOException {
-        PackageBuilder packageBuilder = new PackageBuilder();
-
-//        String ruleConfPath = System.getProperty("vega.rules.config.path");
+///**
+//* Created by dileepa on 4/7/15.
+//*/
+//import lk.vega.charger.util.calculation.PriceCalculator;
+//import org.drools.RuleBase;
+//import org.drools.RuleBaseFactory;
+//import org.drools.WorkingMemory;
+//import org.drools.compiler.DroolsError;
+//import org.drools.compiler.DroolsParserException;
+//import org.drools.compiler.PackageBuilder;
+//import org.drools.compiler.PackageBuilderErrors;
+//import org.drools.rule.Package;
+//import org.junit.Test;
 //
-//        String ruleFile = ruleConfPath+"/priceRule.drl";
-        String ruleFile = "C:\\Users\\dileepa\\Desktop\\WEGA\\DBMODEL\\priceRule.drl";
-        Reader reader = getRuleFileAsReader(ruleFile);
-        packageBuilder.addPackageFromDrl(reader);
-
-        assertNoRuleErrors(packageBuilder);
-
-        return packageBuilder;
-    }
-
-    private Reader getRuleFileAsReader(String ruleFile) {
-
-        InputStream resourceAsStream = null;
-        try
-        {
-            resourceAsStream = new FileInputStream(ruleFile);
-        }
-        catch( FileNotFoundException e )
-        {
-            e.printStackTrace();
-        }
-
-        return new InputStreamReader(resourceAsStream);
-    }
-
-    private RuleBase addRulesToWorkingMemory(PackageBuilder packageBuilder) {
-        RuleBase ruleBase = RuleBaseFactory.newRuleBase();
-        Package rulesPackage = packageBuilder.getPackage();
-        ruleBase.addPackage(rulesPackage);
-
-        return ruleBase;
-    }
-
-    private void assertNoRuleErrors(PackageBuilder packageBuilder) {
-        PackageBuilderErrors errors = packageBuilder.getErrors();
-
-        if (errors.getErrors().length > 0) {
-            StringBuilder errorMessages = new StringBuilder();
-            errorMessages.append("Found errors in package builder\n");
-            for (int i = 0; i < errors.getErrors().length; i++) {
-                DroolsError errorMessage = errors.getErrors()[i];
-                errorMessages.append(errorMessage);
-                errorMessages.append("\n");
-            }
-            errorMessages.append("Could not parse knowledge");
-
-            throw new IllegalArgumentException(errorMessages.toString());
-        }
-    }
-
-    private WorkingMemory initializeMessageObjects(RuleBase ruleBase) {
-        WorkingMemory workingMemory = ruleBase.newStatefulSession();
-
-        createHelloWorld(workingMemory);
-
-        return workingMemory;
-    }
-
-    private void createHelloWorld(WorkingMemory workingMemory) {
-        PriceCalculator helloMessage = new PriceCalculator();
-        helloMessage.setNettRate( 89.67 );
-        helloMessage.setChargingCategory("CAT2");
-        workingMemory.insert(helloMessage);
-    }
-
-}
+//import java.io.FileInputStream;
+//import java.io.FileNotFoundException;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//import java.io.Reader;
+//
+//import static org.hamcrest.core.Is.is;
+//import static org.junit.Assert.assertThat;
+//
+//public class CalculatorTest
+//{
+//
+//    @Test
+//    public void shouldFireHelloWorld() throws IOException, DroolsParserException {
+//        RuleBase ruleBase = initialiseDrools();
+//        WorkingMemory workingMemory = initializeMessageObjects(ruleBase);
+//        int expectedNumberOfRulesFired = 1;
+//
+//        int actualNumberOfRulesFired = workingMemory.fireAllRules();
+//
+//        assertThat(actualNumberOfRulesFired, is(expectedNumberOfRulesFired));
+//    }
+//
+//    private RuleBase initialiseDrools() throws IOException, DroolsParserException {
+//        PackageBuilder packageBuilder = readRuleFiles();
+//        return addRulesToWorkingMemory(packageBuilder);
+//    }
+//
+//    private PackageBuilder readRuleFiles() throws DroolsParserException, IOException {
+//        PackageBuilder packageBuilder = new PackageBuilder();
+//
+////        String ruleConfPath = System.getProperty("vega.rules.config.path");
+////
+////        String ruleFile = ruleConfPath+"/priceRule.drl";
+//        String ruleFile = "C:\\Users\\dileepa\\Desktop\\WEGA\\DBMODEL\\priceRule.drl";
+//        Reader reader = getRuleFileAsReader(ruleFile);
+//        packageBuilder.addPackageFromDrl(reader);
+//
+//        assertNoRuleErrors(packageBuilder);
+//
+//        return packageBuilder;
+//    }
+//
+//    private Reader getRuleFileAsReader(String ruleFile) {
+//
+//        InputStream resourceAsStream = null;
+//        try
+//        {
+//            resourceAsStream = new FileInputStream(ruleFile);
+//        }
+//        catch( FileNotFoundException e )
+//        {
+//            e.printStackTrace();
+//        }
+//
+//        return new InputStreamReader(resourceAsStream);
+//    }
+//
+//    private RuleBase addRulesToWorkingMemory(PackageBuilder packageBuilder) {
+//        RuleBase ruleBase = RuleBaseFactory.newRuleBase();
+//        Package rulesPackage = packageBuilder.getPackage();
+//        ruleBase.addPackage(rulesPackage);
+//
+//        return ruleBase;
+//    }
+//
+//    private void assertNoRuleErrors(PackageBuilder packageBuilder) {
+//        PackageBuilderErrors errors = packageBuilder.getErrors();
+//
+//        if (errors.getErrors().length > 0) {
+//            StringBuilder errorMessages = new StringBuilder();
+//            errorMessages.append("Found errors in package builder\n");
+//            for (int i = 0; i < errors.getErrors().length; i++) {
+//                DroolsError errorMessage = errors.getErrors()[i];
+//                errorMessages.append(errorMessage);
+//                errorMessages.append("\n");
+//            }
+//            errorMessages.append("Could not parse knowledge");
+//
+//            throw new IllegalArgumentException(errorMessages.toString());
+//        }
+//    }
+//
+//    private WorkingMemory initializeMessageObjects(RuleBase ruleBase) {
+//        WorkingMemory workingMemory = ruleBase.newStatefulSession();
+//
+//        createHelloWorld(workingMemory);
+//
+//        return workingMemory;
+//    }
+//
+//    private void createHelloWorld(WorkingMemory workingMemory) {
+//        PriceCalculator helloMessage = new PriceCalculator();
+//        helloMessage.setNettRate( 89.67 );
+//        helloMessage.setChargingCategory("CAT2");
+//        workingMemory.insert(helloMessage);
+//    }
+//
+//}
