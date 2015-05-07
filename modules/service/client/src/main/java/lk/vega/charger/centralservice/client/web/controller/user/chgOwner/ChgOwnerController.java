d3a9a5ad8749a771e2ab07b890720487a3cf720b
dileepa <dileepasisila@gmail.com>
2015-05-07 19:02:04 +0530

    WSO2 IS implementation

--- modules/service/client/src/main/java/lk/vega/charger/centralservice/client/web/controller/user/chgOwner/ChgOwnerController.java
+++ modules/service/client/src/main/java/lk/vega/charger/centralservice/client/web/controller/user/chgOwner/ChgOwnerController.java
@@ -2,6 +2,7 @@ package lk.vega.charger.centralservice.client.web.controller.user.chgOwner;
 
 import lk.vega.charger.centralservice.client.web.domain.user.chgOwner.ChgOwnerBean;
 import lk.vega.charger.core.ChgUser;
+import lk.vega.charger.core.UserRoles;
 import lk.vega.charger.userManagement.UserHandler;
 import lk.vega.charger.util.ChgResponse;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.wso2.carbon.um.ws.service.AddUser;
+import org.wso2.carbon.um.ws.service.RemoteUserStoreManagerServicePortType;
+import org.wso2.carbon.um.ws.service.RemoteUserStoreManagerServiceUserStoreException_Exception;
 
 /**
  * Intelij Idea IDE
@@ -47,29 +51,28 @@ public class ChgOwnerController
         {
             e.printStackTrace();
         }
-//        if (chgResponse.isSuccess())
-//        {
-//            UserAdminPortType userAdminService = (UserAdminPortType)chgResponse.getReturnData();
-//            try
-//            {
-//                AddUser newUser = new AddUser();
-//                newUser.setUserName( chgOwnerBean.getUserName() );
-//                userAdminService.addUser( chgOwnerBean.getUserName(), chgOwnerBean.getPassword(),new String[]{UserRoles.CHG_OWNER},null,null );
-//            }
-//            catch( RemoteException e )
-//            {
-//                e.printStackTrace();
-//            }
-//            catch( UserAdminUserAdminException e )
-//            {
-//                e.printStackTrace();
-//            }
-//            catch( Exception e )
-//            {
-//                e.printStackTrace();
-//            }
-//
-//        }
+        if (chgResponse.isSuccess())
+        {
+            RemoteUserStoreManagerServicePortType remoteUserStoreManagerService = (RemoteUserStoreManagerServicePortType)chgResponse.getReturnData();
+            try
+            {
+                AddUser newUser = new AddUser();
+                newUser.setUserName( chgOwnerBean.getUserName() );
+                newUser.setRequirePasswordChange( false );
+                newUser.setCredential( chgOwnerBean.getPassword() );
+                newUser.getRoleLists().add( UserRoles.CHG_OWNER );
+                remoteUserStoreManagerService.addUser( newUser );
+            }
+            catch( RemoteUserStoreManagerServiceUserStoreException_Exception e )
+            {
+                e.printStackTrace();
+            }
+            catch( Exception e )
+            {
+                e.printStackTrace();
+            }
+
+        }
 
         return new ModelAndView(  );
     }
--- modules/service/core/src/main/java/lk/vega/charger/userManagement/UserHandler.java
+++ modules/service/core/src/main/java/lk/vega/charger/userManagement/UserHandler.java
@@ -3,7 +3,6 @@ package lk.vega.charger.userManagement;
 import lk.vega.charger.util.ChgResponse;
 import lk.vega.charger.util.SOAPLoggingHandler;
 import org.wso2.carbon.um.ws.service.RemoteUserStoreManagerServicePortType;
-import org.wso2.carbon.um.ws.service.RemoteUserStoreManagerServiceUserStoreException_Exception;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
@@ -35,14 +34,14 @@ public class UserHandler
 {
 
     public static final String USER_ADMIN_SERVICE = "https://192.168.1.32:9443/services/UserAdmin?wsdl";
-    public static final String REMOTE_USER_STORE_MANAGER_SERVICE = "https://localhost:9443/services/RemoteUserStoreManagerService?wsdl";
+    public static final String REMOTE_USER_STORE_MANAGER_SERVICE = "https://192.168.1.32:9443/services/RemoteUserStoreManagerService?wsdl";
     public static final String REMOTE_USER_STORE_MANAGER_NAMESPACE_URI = "http://service.ws.um.carbon.wso2.org";
     public static final String REMOTE_USER_STORE_MANAGER_SOAP11_PORT = "RemoteUserStoreManagerServiceHttpsSoap11Endpoint";
     public static final String REMOTE_USER_STORE_MANAGER_SOAP12_PORT = "RemoteUserStoreManagerServiceHttpsSoap12Endpoint";
     public static final String REMOTE_USER_STORE_MANAGER_HTTPS_PORT = "RemoteUserStoreManagerServiceHttpsEndpoint";
     public static final String USER_ADMIN_NAMESPACE_URI = "http://mgt.user.carbon.wso2.org";
     public static final String USER_ADMIN_USERNAME = "admin";
-    public static final String USER_ADMIN_PASSWORD = "vega321";
+    public static final String USER_ADMIN_PASSWORD = "admin";
 
     private static void disableSslVerification()
     {
@@ -92,6 +91,24 @@ public class UserHandler
         }
     }
 
+    public static void main( String[] args )
+    {
+        ChgResponse chgResponse = new ChgResponse(  );
+        try
+        {
+            chgResponse = connectToRemoteUserStoreManagerService();
+        }
+        catch( Exception e )
+        {
+
+            e.printStackTrace();
+        }
+
+        if (chgResponse.isSuccess())
+        {
+            System.out.println("success");
+        }
+    }
 
     public static ChgResponse connectToRemoteUserStoreManagerService() throws Exception
     {
@@ -111,7 +128,9 @@ public class UserHandler
 
 
         QName serviceQName = new QName( REMOTE_USER_STORE_MANAGER_NAMESPACE_URI, "RemoteUserStoreManagerService" );
-        ServiceDelegate delegate = Provider.provider().createServiceDelegate( urlWsdl, serviceQName, RemoteUserStoreManagerServicePortType.class );
+        Class provideClass = Class.forName( "com.sun.xml.internal.ws.spi.ProviderImpl" );
+        Provider provider = (Provider) provideClass.newInstance();
+        ServiceDelegate delegate = provider.createServiceDelegate( urlWsdl, serviceQName, RemoteUserStoreManagerServicePortType.class );
 
         QName portQName = new QName( REMOTE_USER_STORE_MANAGER_NAMESPACE_URI, REMOTE_USER_STORE_MANAGER_SOAP11_PORT );
         RemoteUserStoreManagerServicePortType remoteUserStoreManagerService = delegate.getPort( portQName, RemoteUserStoreManagerServicePortType.class );
@@ -123,171 +142,32 @@ public class UserHandler
         handlerChain.add( new SOAPLoggingHandler() );
         ( (BindingProvider) remoteUserStoreManagerService ).getBinding().setHandlerChain( handlerChain );
 
-        try {
-            List<String> users = remoteUserStoreManagerService.listUsers("*", 100);
-            for(String user: users){
-                System.out.println("User : " + user);
-            }
-        } catch (RemoteUserStoreManagerServiceUserStoreException_Exception e) {
-            e.printStackTrace();
-        }
+//        try
+//        {
+//            List<String> users = remoteUserStoreManagerService.listUsers( "*", 100 );
+//            for( String user : users )
+//            {
+//                System.out.println( "User : " + user );
+//            }
+//        }
+//        catch( RemoteUserStoreManagerServiceUserStoreException_Exception e )
+//        {
+//            e.printStackTrace();
+//        }
 
-//        if( remoteUserStoreManagerService != null )
-//        {
-//            chgResponse.setNo( ChgResponse.SUCCESS );
-//            chgResponse.setMsg( "Connection established Successfully" );
-//            chgResponse.setReturnData( remoteUserStoreManagerService );
-//        }
-//        else
-//        {
-//            chgResponse.setNo( ChgResponse.ERROR );
-//            chgResponse.setMsg( "Connection Failed" );
-//        }
+        if( remoteUserStoreManagerService != null )
+        {
+            chgResponse.setNo( ChgResponse.SUCCESS );
+            chgResponse.setMsg( "Connection established Successfully" );
+            chgResponse.setReturnData( remoteUserStoreManagerService );
+        }
+        else
+        {
+            chgResponse.setNo( ChgResponse.ERROR );
+            chgResponse.setMsg( "Connection Failed" );
+        }
         return chgResponse;
     }
 
-
-//    private String serverUrl = "https://localhost:9443/services/";
-//
-//    private AuthenticationAdminStub authstub = null;
-//    private ConfigurationContext ctx;
-//    private String authCookie = null;
-//    private WSUserStoreManager remoteUserStoreManager = null;
-//
-//
-//    /**
-//     * Initialization of environment
-//     *
-//     * @throws Exception
-//     */
-//    public RemoteUMClient() throws Exception {
-//        ctx = ConfigurationContextFactory.createConfigurationContextFromFileSystem(null, null);
-//        String authEPR = serverUrl + "AuthenticationAdmin";
-//        authstub = new AuthenticationAdminStub(ctx, authEPR);
-//        ServiceClient client = authstub._getServiceClient();
-//        Options options = client.getOptions();
-//        options.setManageSession(true);
-//        options.setProperty(org.apache.axis2.transport.http.HTTPConstants.COOKIE_STRING, authCookie);
-//    }
-//
-//    /**
-//     * Authenticate to carbon as admin user and obtain the authentication cookie
-//     *
-//     * @param username
-//     * @param password
-//     * @return
-//     * @throws Exception
-//     */
-//    public String login(String username, String password) throws Exception {
-//        //String cookie = null;
-//        boolean loggedIn = authstub.login(username, password, "localhost");
-//        if (loggedIn) {
-//            System.out.println("The user " + username + " logged in successfully.");
-//            authCookie = (String) authstub._getServiceClient().getServiceContext().getProperty(
-//                    HTTPConstants.COOKIE_STRING);
-//        } else {
-//            System.out.println("Error logging in " + username);
-//        }
-//        return authCookie;
-//    }
-//
-//    /**
-//     * create web service client for RemoteUserStoreManager service from the wrapper api provided
-//     * in carbon - remote-usermgt component
-//     *
-//     * @throws UserStoreException
-//     */
-//    public void createRemoteUserStoreManager() throws UserStoreException {
-//
-//        remoteUserStoreManager = new WSUserStoreManager(serverUrl, authCookie, ctx);
-//    }
-//
-//    /**
-//     * Add a user store to the system.
-//     *
-//     * @throws UserStoreException
-//     */
-//    public void addUser(String userName, String password) throws UserStoreException {
-//
-//        remoteUserStoreManager.addUser(userName, password, null, null, null);
-//        System.out.println("Added user: " + userName);
-//    }
-//
-//    /**
-//     * Add a role to the system
-//     *
-//     * @throws Exception
-//     */
-//    public void addRole(String roleName) throws UserStoreException {
-//        remoteUserStoreManager.addRole(roleName, null, null);
-//        System.out.println("Added role: " + roleName);
-//    }
-//
-//    /**
-//     * Add a new user by assigning him to a new role
-//     *
-//     * @throws Exception
-//     */
-//    public void addUserWithRole(String userName, String password, String roleName)
-//            throws UserStoreException {
-//        remoteUserStoreManager.addUser(userName, password, new String[]{roleName}, null, null);
-//        System.out.println("Added user: " + userName + " with role: " + roleName);
-//    }
-//
-//    /**
-//     * Retrieve all the users in the system
-//     *
-//     * @throws Exception
-//     */
-//    public String[] listUsers() throws UserStoreException {
-//        return remoteUserStoreManager.listUsers("*", -1);
-//    }
-//
-//    /**
-//     * Delete an exisitng user from the system
-//     *
-//     * @throws Exception
-//     */
-//    public void deleteUser(String userName) throws UserStoreException {
-//        remoteUserStoreManager.deleteUser(userName);
-//        System.out.println("Deleted user:" + userName);
-//    }
-//
-//    public static void main(String[] args) throws Exception {
-//        //set trust store properties required in SSL communication.
-//        String is_Home = ".." + File.separator + ".." + File.separator;
-//        System.setProperty("javax.net.ssl.trustStore", is_Home + "repository" + File.separator + "resources" +
-//                File.separator + "security" + File.separator + "wso2carbon.jks");
-//        System.setProperty("javax.net.ssl.trustStorePassword", "wso2carbon");
-//
-//        RemoteUMClient remoteUMClient = new RemoteUMClient();
-//        //log in as admin user and obtain the cookie
-//        remoteUMClient.login("admin", "admin");
-//
-//        /*Create client for RemoteUserStoreManagerService and perform user management operations*/
-//
-//        //create web service client
-//        remoteUMClient.createRemoteUserStoreManager();
-//        //add a new user to the system
-//        remoteUMClient.addUser("kamal", "kamal");
-//        //add a role to the system
-//        remoteUMClient.addRole("eng");
-//        //add a new user with a role
-//        remoteUMClient.addUserWithRole("saman", "saman", "eng");
-//        //print a list of all the users in the system
-//        String[] users = remoteUMClient.listUsers();
-//        System.out.println("List of users in the system:");
-//        for (String user : users) {
-//            System.out.println(user);
-//        }
-//        //delete an existing user
-//        remoteUMClient.deleteUser("kamal");
-//        //print the current list of users
-//        String[] userList = remoteUMClient.listUsers();
-//        System.out.println("List of users in the system currently:");
-//        for (String user : userList) {
-//            System.out.println(user);
-//        }
-//    }
 
 }
